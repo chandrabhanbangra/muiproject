023d715c48e72f319d23c6f283ea90ad2a4f3ac7
chandrabhan <crbgithub@gmail.com>
2023-12-01 18:25:10 +0530

    crb

--- demo/src/Componate/Products.jsx
+++ demo/src/Componate/Products.jsx
@@ -2,120 +2,148 @@ import CategoryPage from "./CategoryPage";
 import React, { useState } from "react";
 import axios from "axios";
 import { useEffect } from "react";
-import { Grid, Paper, Typography, CardMedia,Button, Container, TextField,Stack, Toolbar} from "@mui/material";
+import {
+  Grid,
+  Paper,
+  Typography,
+  CardMedia,
+  Button,
+  Container,
+  TextField,
+  Stack,
+  Toolbar,
+} from "@mui/material";
 import CartButton from "./cartButton";
-import '../index.css'
+import "../index.css";
 import Navbar from "./Navbar";
-import { Routes,Route ,BrowserRouter, Link, useParams} from "react-router-dom";
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  Link,
+  useParams,
+} from "react-router-dom";
 import ItemPage from "./ItemPage";
-import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded';
+import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded";
+
+
 
 
 const Products = (props) => {
-  const[myData, setMyData] = useState([]);
-  const [wishlist, setWishlist] =useState([]);
-  const [wishlistCount, setWishlistCount] = useState()
-  
-  
+  const [myData, setMyData] = useState([]);
+  const [wishlist, setWishlist] = useState([]);
+  const [wishlistCount, setWishlistCount] = useState();
 
-  
   React.useEffect(() => {
-    axios.get("https://dummyjson.com/products")
-    .then((res) => {
-        setMyData(res.data.products);
+    axios.get("https://dummyjson.com/products").then((res) => {
+      setMyData(res.data.products);
     });
-  },[]);
-
-
-  
-  
-
-
-  
-  
-  
+  }, []);
+  console.log(myData)
+ console.log(typeof myData)
   useEffect(() => {
-   
-    
-    localStorage.setItem('wishlist',  JSON.stringify(wishlist));
-    
+    localStorage.setItem("wishlist", JSON.stringify(wishlist));
   }, [wishlist]);
-  
-  useEffect(() => {
-   
-    
-    localStorage.setItem('wishlist',  JSON.stringify(wishlist));
-    
-  }, );
-  
- 
-  
-   
-  
-  
-  
+
   // useEffect(() => {
   //   const items = JSON.parse(localStorage.getItem('wishlist'));
   //   if (wishlist) {
   //     setWishlist(wishlist);
   //   }
   // }, []);
-  const wishlistAddHandler=(event, index )=>{
-    // console.log(index)
+  const wishlistAddHandler = (event, index) => {
+    setWishlistCount(wishlistCount + 1);
 
-    setWishlistCount(wishlistCount +1)
-    
-    setWishlist(index)
-    
-  }
-  
-  
-  
+    setWishlist((prev) => {
+      let index1 = index;
+      return [index1, ...prev];
+    });
+  };
+  console.log(wishlist);
 
- const categorydata=myData.map((item, index)=>{
-  
-    return(
-     <Grid item xs={12} sm={5} xl={2} key={index} sx={{ "&:hover": {
-          
-      transform:"scale(1.05)",
-     }}}  >
-     <Paper elevation={7} sx={{paddingBottom:"20px", paddingX:"10px",paddingTop:"10px",height:"380px" }} >
-      <Toolbar  >
-      
-     <CardMedia component="img"   image={item.thumbnail} sx={{
-      height:"300px", 
-    
-        }}  />
-     
-     </Toolbar>
-     <Typography variant="h6" className="categoryHeading" sx={{ whiteSpace:"nowrap"}}>
-       {item.title}
-     </Typography>
-     <Typography sx={{display:"flex",justifyContent:"space-between"}}>
+  const categorydata = myData.map((item, index) => {
+    return (
+      <Grid
+        item
+        xs={12}
+        sm={5}
+        xl={2}
+        key={index}
+        sx={{
+          "&:hover": {
+            transform: "scale(1.05)",
+          },
+        }}
+      >
+        <Paper
+          elevation={7}
+          sx={{
+            paddingBottom: "20px",
+            paddingX: "10px",
+            paddingTop: "10px",
+            height: "380px",
+          }}
+        >
+          <Toolbar>
+            <CardMedia
+              component="img"
+              image={item.thumbnail}
+              sx={{
+                height: "300px",
+              }}
+            />
+          </Toolbar>
+          <Typography
+            variant="h6"
+            className="categoryHeading"
+            sx={{ whiteSpace: "nowrap" }}
+          >
+            {item.title}
+          </Typography>
+          <Typography sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Button
+              to={{
+                pathname: `/ItemPage/${index}`,
+                query: index,
+                search: `?title=${item.title}`,
+              }}
+              component={Link}
+              itemID={index}
+            >
+              {" "}
+              BUY NOW{" "}
+            </Button>
 
-      
-     <Button  to={{pathname: `/ItemPage/${index}`, query: index,search: `?title=${item.title}`}} component={Link}   itemID={index}> BUY NOW </Button>
-     
-     <FavoriteBorderRoundedIcon key={index}    onClick={(event)=>wishlistAddHandler(event , index)} color="primary" sx={{width:"60px" ,height:"40px",}}/>
-      
-     </Typography>
-     </Paper>
-     </Grid>
-     
-    )
-  })
-  
-  
- 
-  return (
-      
-      <>
-      <Grid  container xs={12} sm={12} xl={12}    sx={{justifyContent:"center",    marginTop: "70px",   columnGap:"15px", rowGap:"30px", padding:"20px" }} >
-      {categorydata}
+            <FavoriteBorderRoundedIcon
+              key={index}
+              onClick={(event) => wishlistAddHandler(event, index)}
+              color="primary"
+              sx={{ width: "60px", height: "40px" }}
+            />
+          </Typography>
+        </Paper>
       </Grid>
-      
-      </>
-     
+    );
+  });
+
+  return (
+    <>
+      <Grid
+        container
+        xs={12}
+        sm={12}
+        xl={12}
+        sx={{
+          justifyContent: "center",
+          marginTop: "70px",
+          columnGap: "15px",
+          rowGap: "30px",
+          padding: "20px",
+        }}
+      >
+        {categorydata}
+      </Grid>
+    </>
   );
 };
 export default Products;
--- demo/src/Componate/Wishlist.jsx
+++ demo/src/Componate/Wishlist.jsx
@@ -1,24 +1,56 @@
-import { Typography } from '@mui/material'
+import { Table, TableCell, TableContainer, Typography,TableRow, Toolbar, Container } from '@mui/material'
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { json } from 'react-router-dom'
 
-const Wishlist = () => {
-      
+const Wishlist =  () => {
+  let[wishlistItems, setWishlistItems]= useState([])   
     
-
-    // useEffect(()=>{
-    //     axios.get("https://dummyjson.com/products")
-    //     .then((res)=>{
-              
-    //     }[])
-    // })
+  let localWishList = JSON.parse(localStorage.getItem('wishlist'))
+  //  console.log(wishlistItems)
+  for(let i = 0; i < localWishList.length; i++){
+    useEffect(()=>{
+      axios.get(`https://dummyjson.com/products/${localWishList[i]}`)
+      .then((res)=>{
+       setWishlistItems(res.data)
+      //  console.log(res.data)
+      })
+   },[setWishlistItems])
+    }
+  
+  //   object.
+ 
+ const myde =Object.entries(wishlistItems);
+ console.log(myde)
   return (
-     <>
-     <Typography variant='h1'>dfdf</Typography>
-     <Typography variant='h1'>dfdf</Typography>
-     <Typography variant='h1'>dfdf</Typography>
-
-     </>
+    <>
+    <Container sx={{marginTop:"80px"}}>
+    <Typography variant='h5'>Wishlist</Typography>
+    <TableContainer >
+       <Table>
+        <TableRow>
+          <TableCell>Products</TableCell>
+          <TableCell>Price</TableCell>
+          <TableCell>QTY</TableCell>
+          <TableCell>Remove</TableCell>
+          
+        </TableRow>
+        {myde.map((items ,index ) => {
+        
+        return(
+                <>
+                <TableRow >
+                  <TableCell key={index}>{items[1]}</TableCell>
+                  {/* <TableCell key={index}>{items[2]}</TableCell>
+                  <TableCell key={index}>{items[3]}</TableCell> */}
+                </TableRow>
+                </>
+               )
+      })}
+       </Table>
+      </TableContainer>
+      </Container> 
+    </>
   )
 }
 
